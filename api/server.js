39bc37d4b5d02a23b6dfb9b338bea702baf6a45e
Francis Bonacic <francisbonacic@icloud.com>
2021-02-10 16:01:49 -0800

    just starting out

--- api/server.js
+++ api/server.js
@@ -2,8 +2,9 @@ const express = require('express');
 
 const server = express();
 
-// remember express by default cannot parse JSON in request bodies
 
+// remember express by default cannot parse JSON in request bodies
+server.use(express.json())
 // global middlewares and routes need to be connected here
 
 server.get('/', (req, res) => {
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // require your server and launch it
+
+const server = require('./api/server')
+
+port = process.env.PORT || 5000  //enviroment varibale
+
+server.listen(port, () => {
+    console.log(`server running on port ${port}`)
+})
